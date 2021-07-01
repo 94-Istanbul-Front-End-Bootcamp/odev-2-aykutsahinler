030dc4b15808264ca07b06a45909be085dbd7504
aykutsahinler <aykut-sahinler@hotmail.com>
2021-07-01 14:22:23 +0300

    Filter data separately

--- index.js
+++ index.js
@@ -1,50 +1,99 @@
 let data = [];
 
+// Query Selectors
+
+let inputs = document.querySelectorAll(".input-container");
+let ageCheckBox = document.getElementById("age");
+let isActiveCheckbox = document.getElementById("isActive");
+let nameInput = document.getElementById("name");
+
+// ---- Functions ---- //
+
+// Fetch Data
 const fetchData = () => {
-    //verinin çekildiği yer
-    fetch("data.json")
-    .then(response => {
-        return response.json();
+  //verinin çekildiği yer
+  fetch("data.json")
+    .then((response) => {
+      return response.json();
     })
-    .then(responseData => {
-        //json'dan okunan verinin data array'ine atanması
-        data = responseData;
+    .then((responseData) => {
+      //json'dan okunan verinin data array'ine atanması
+      data = responseData;
 
-        //veri geldikten sonra filtreleme butonu görünür olsun
-        let filterButton = document.querySelector("#filterButton");
-        filterButton.setAttribute("style", "");
+      //veri geldikten sonra filtreleme butonu görünür olsun
+      let filterButton = document.querySelector("#filterButton");
 
-        //verinin html içerisinde listelendiği fonksiyon
-        listData(responseData);
+      filterButton.setAttribute("style", "");
+
+      //verinin html içerisinde listelendiği fonksiyon
+      listData(responseData);
     })
-    .catch(err => {
-        //hata yönetimi
-        console.log(err)
-    })
+    .catch((err) => {
+      //hata yönetimi
+      console.log(err);
+    });
+};
+
+// Filters
+
+function filterByAge() {
+    if(ageCheckBox.checked) {
+        let filterAge = data.filter((person) => person.age > 18);
+        listData(filterAge);
+    }
+    return true
 }
 
-//verinin ul tag'i içerisinde listelenmesini sağlayan fonksiyon
-const listData = (data) => {
-    let list = document.querySelector(".list");
-    list.innerHTML = data.map(element => {
-        return `
-        <li id=${element.id}>
-            <span class='bold'>name:</span> ${element.name}
-            <span class='bold'>email:</span> ${element.email}
-        </li>
-        `;
-    })
-}
-
-//verinin filtrelenmesini sağlayan fonksiyon
-//TODO
-const filterData = (filter) => {
-    switch (filter) {
-        case "isActive":
-            let filteredData = data.filter(element => element.isActive === true);
-            listData(filteredData);
-            break;
-        default:
-            break;
+function filterByIsActive() {
+    if(isActiveCheckbox.checked) {
+        let filterIsActive = data.filter((element) => element.isActive === true);
+        listData(filterIsActive);
+        return true
     }
 }
+
+function filterByName() {
+    if(nameInput.value.length != "") {
+        let filterName = data.filter((name) => name.name[0] === nameInput.value.toUpperCase());
+        listData(filterName);
+    }
+
+    return true
+}
+
+
+function filterData() {
+    filterByAge();
+    filterByIsActive();
+    filterByName();
+}
+
+// Manipulating DOM
+const listData = (data) => {
+  let list = document.querySelector(".list");
+  list.innerHTML = data.map((element) => {
+    return `
+        <li id=${element.id}>
+            <strong><span>name:</span></strong> ${element.name} <br>
+            <strong><span>isActive:</span></strong> ${element.isActive}<br>
+            <strong><span>age:</span></strong> ${element.age}<br>
+        </li>
+        `;
+  });
+};
+
+//verinin filtrelenmesini sağlayan fonksiyon
+
+
+// const filterData = (filter) => {
+//     console.log(filter);
+//   switch (filter) {
+//     case "isActive":
+//       let filterIsActive = data.filter((element) => element.age >= 18);
+//       listData(filterIsActive);
+//       break;
+//     default:
+//         break;
+
+//   }
+// };
